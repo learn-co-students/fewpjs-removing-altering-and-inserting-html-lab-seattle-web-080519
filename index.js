b1c9f18c990b399e525ee1ed00682b8bf410309c
AKhan-CIR <akhan@codeinreach.com>
2019-09-16 15:57:08 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your code here!
+
+
+document.addEventListener("DOMContentLoaded", function(){
+    
+
+    
+    });
+
+
+    
+
+    let main = document.querySelector('main#main');
+    main.remove()
+
+    let newHeader = document.createElement('h1');
+    newHeader.innerHTML = "ASIF-KHAN is the champion";
+    newHeader.setAttribute('id', 'victory');
+    document.body.appendChild(newHeader)  
+
