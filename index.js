6e6bd2bee36e19c7ad0a2e308d7fc99649132d24
github username <github email address>
2019-09-16 17:38:42 -0700

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+// creating DOM elements programmatically
 document.querySelector("main#main").remove();
-
+// adding elements to DOM
 const newHeader = document.createElement("h1");
 newHeader.id = "victory";
+// updating elements via innerHTML
 newHeader.innerHTML = "Grace is the champion";
+// change properties on DOM nodes
 newHeader.className = "victory";
